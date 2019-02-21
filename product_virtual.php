8c51e7ac4a277da44875be835392352f8d2461e8
AiOZ [ Afrasiab ] <ali.afrasiab360@gmail.com>
2019-02-21 17:54:27 +0100

    Update product_virtual.php

--- product_virtual.php
+++ product_virtual.php
@@ -1,4 +1,47 @@
 <?php
+
+if(isset($_GET)) 
+{
+    $idParent = null;
+    
+    if(isset($_GET['id'])) {
+        $idParent = $_GET['id'];
+    }
+    $idParent = 1;
+	
+	// no module_part
+    if($idParent == null) {
+        echo 'no_product';
+        return;
+    }
+	$pdtVirtuels = "";
+	g_a_l($idParent,$pdtVirtuels);
+    echo(json_encode($pdtVirtuels));
+}
+
+function g_a_l($id, &$table='')
+{
+	$connex	= mysqli_connect("localhost","root","admin","soif_express");
+	if(mysqli_connect_errno())
+	{
+		print "Failed connect " . mysqli_connect_error();
+	}
+	$sql		= "SELECT * FROM llx_product_association pa, llx_product p where pa.fk_product_pere = p.rowid AND pa.fk_product_fils =".$id." ";
+	$result		= mysqli_query($connex,$sql);
+	$rowcount	= mysqli_num_rows($result);
+	
+	if($rowcount > 0)
+	{
+		while( $ligne = mysqli_fetch_array($result) )
+		{
+			$table[] = $ligne;
+			g_a_l($ligne[1],$table);
+		}
+	}	
+	mysqli_close($connex);
+}
+
+/*
 if(isset($_GET)) {
     $idParent = null;
     
@@ -13,12 +56,19 @@ if(isset($_GET)) {
     }
 
     $db = new PDO("mysql:host=localhost; dbname=soif_express", "root", "admin");
-    $query = "SELECT * FROM llx_product_association pa, llx_product p where pa.fk_product_pere = p.rowid AND pa.fk_product_fils = :id";
     
+    $query = "SELECT * FROM llx_product_association pa, llx_product p where pa.fk_product_pere = p.rowid AND pa.fk_product_fils = :id";
     $stmt = $db->prepare($query);
     $stmt->execute(['id' => $idParent]); 
-    $pdtVirtuels = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+    $pdtVirtuels[] = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+    $query = "SELECT * FROM llx_product_association pa, llx_product p where pa.fk_product_pere = p.rowid AND pa.fk_product_fils = :id";
+    $stmt = $db->prepare($query);
+    $stmt->execute(['id' => $idParent]); 
     
     echo(json_encode($pdtVirtuels));
     
 }
+
+*/
